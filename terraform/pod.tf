76d30a6cf8dab69e1b8da9b6fccdf5dd971fcb1d
yb2411 <yassine.bergui@outlook.fr>
2023-07-20 00:26:34 +0200

    2 replicas

--- terraform/pod.tf
+++ terraform/pod.tf
@@ -1,18 +1,36 @@
-resource "kubernetes_pod" "webserver" {
+resource "kubernetes_deployment" "webserver_deployment" {
   metadata {
-    name = "webserver"
+    name = "webserver-deployment"
     labels = {
       app = "webserver"
     }
   }
 
   spec {
-    container {
-      image = "117698939310.dkr.ecr.eu-west-1.amazonaws.com/zumo_website:latest"
-      name  = "zumo-webserver"
-      
-      port {
-        container_port = 80
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "webserver"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "webserver"
+        }
+      }
+      spec {
+        container {
+          image = "117698939310.dkr.ecr.eu-west-1.amazonaws.com/zumo_website:latest"
+          name  = "zumo-webserver"
+
+          port {
+            container_port = 80
+          }
+        }
+        image_pull_secrets {
+          name = "ecr-credentials"
+        }
       }
     }
   }
